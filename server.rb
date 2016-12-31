e9538d1257ff033b2b4760dedd42595de389255f
Lev Kravinsky <kravinskylev@gmail.com>
2016-12-30 16:35:04 -0800

    remove old oauth strategy

--- server.rb
+++ server.rb
@@ -1,18 +1,15 @@
 require 'google/cloud/translate'
-require 'google/api_client/client_secrets'
 require 'omniauth'
 require 'omniauth-google-oauth2'
 require 'sinatra'
 require 'pry'
-require 'net/http'
 require './util/api_wrapper'
 
 enable :sessions
 set :session_secret, ENV['SESSION_SECRET']
 
 use OmniAuth::Builder do
-  # For additional provider examples please look at 'omni_auth.rb'
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {access_type: "offline", prompt: "consent", scope: 'userinfo.email,calendar'}
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {access_type: "offline", prompt: "consent", scope: 'userinfo.email'}
 end
 
 translate = Google::Cloud::Translate.new
@@ -59,49 +56,6 @@ post '/your-charge-code' do
   customer = stripe_api_wrapper.create_customer(token)
 
   erb :index, :layout => :nav
-end
-
-get '/test-auth' do
-  auth_uri = auth_client.authorization_uri.to_s
-  redirect to(auth_uri)
-end
-
-get '/app' do
-  unless session.has_key?(:credentials)
-   redirect to('/oauth2callback')
-  end
-  erb :demo, :layout => :nav
-end
-
-get '/oauth2callback' do
-  json_secrets = JSON.parse(ENV['GOOGLE_CLIENT_SECRETS'])
-  client_secrets = Google::APIClient::ClientSecrets.new(json_secrets)
-  auth_client = client_secrets.to_authorization
-  auth_client.update!(
-    scope: ['email', 'profile'],
-    redirect_uri: 'https://typelang.herokuapp.com/oauth2callback'
-  )
-  if request['code'] == nil
-    auth_uri = auth_client.authorization_uri.to_s
-    redirect to(auth_uri)
-  else
-    auth_client.code = request['code']
-    auth_client.fetch_access_token!
-    auth_client.client_secret = nil
-    session[:credentials] = auth_client.to_json
-    stripe_api_wrapper.check_for_subscription()
-    redirect to('/')
-  end
-end
-
-get '/creds' do
-  session[:credentials]
-end
-
-get '/auth' do
-  uri = URI("https://www.googleapis.com/oauth2/v1/userinfo?access_token=#{session[:credentials]['access_token']}")
-  profile = Net::HTTP.get(uri)
-  puts profile
 end
 
 get '/auth/failure' do
