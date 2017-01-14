dfbfb14f6e29792f5192aee377ded340fc34d2bd
Lev Kravinsky <kravinskylev@gmail.com>
2017-01-14 16:50:17 -0500

    truncate words on categories

--- public/js/category.js
+++ public/js/category.js
@@ -1,1 +1,13 @@
-console.log('category')
+document.querySelectorAll('.lesson').forEach(function(lesson) {
+  var words = lesson.textContent.split(' ')
+  var truncatedWords = ''
+  var newWord = ''
+  var index = 0
+  newWord = ` ${words[index]}`
+  while (truncatedWords.length + newWord.length < 25) {
+    truncatedWords += newWord
+    index++
+    newWord = ` ${words[index]}`
+  }
+  lesson.textContent = truncatedWords + '...'
+})
